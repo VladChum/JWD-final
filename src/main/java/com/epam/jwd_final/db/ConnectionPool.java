0b2686ff5f1c46f14021a22d6ab12816f09b136c
vlad <DegaDega001@mail.ru>
2021-04-17 03:33:43 +0300

    update connection pool

--- src/main/java/com/epam/jwd_final/db/ConnectionPool.java
+++ src/main/java/com/epam/jwd_final/db/ConnectionPool.java
@@ -1,6 +1,8 @@
 package com.epam.jwd_final.db;
 
 import com.epam.jwd_final.exception.ConnectionPoolException;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.Driver;
@@ -13,10 +15,11 @@ import java.util.concurrent.LinkedBlockingDeque;
 public enum ConnectionPool {
     INSTANCE;
 
+    private static final Logger LOGGER = Logger.getLogger(ConnectionPool.class);
     private final int CONNECTION_POOL_SIZE = 16;
-    private  String url;
-    private  String user;
-    private  String password;
+    private final String URL;
+    private final String USER;
+    private final String PASSWORD;
 
     private BlockingQueue<ConnectionProxy> freeConnections;
     private Queue<ConnectionProxy> givenAwayConnections;
@@ -24,26 +27,25 @@ public enum ConnectionPool {
     ConnectionPool() {
         DBResourceManager dbResourceManager = DBResourceManager.getInstance();
 
-        this.url = dbResourceManager.getValue(DBParameter.DB_URL);
-        this.user = dbResourceManager.getValue(DBParameter.DB_USER);
-        this.password = dbResourceManager.getValue(DBParameter.DB_PASSWORD);
+        this.URL = dbResourceManager.getValue(DBParameter.DB_URL);
+        this.USER = dbResourceManager.getValue(DBParameter.DB_USER);
+        this.PASSWORD = dbResourceManager.getValue(DBParameter.DB_PASSWORD);
         init();
-        givenAwayConnections = new ArrayDeque<>(CONNECTION_POOL_SIZE);
+        givenAwayConnections = new ArrayDeque<>();
     }
 
-    private BlockingQueue<ConnectionProxy> init() {
+    private void init() {
         freeConnections = new LinkedBlockingDeque<>(CONNECTION_POOL_SIZE);
         Connection connection;
         try {
             for (int i = 0; i < CONNECTION_POOL_SIZE; i++) {
-                connection = DriverManager.getConnection(url , user, password);
-                freeConnections.offer(new ConnectionProxy(connection));
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                freeConnections.add(new ConnectionProxy(connection));
             }
         } catch (SQLException e) {
-//            LOGGER.log(Level.FATAL, "ConnectionPool was not initialized", e);
+            LOGGER.log(Level.FATAL, "ConnectionPool was not initialized", e);
             throw new RuntimeException("ConnectionPool was not initialized", e);
         }
-        return freeConnections;
     }
 
     public Connection getConnection() {
@@ -58,18 +60,17 @@ public enum ConnectionPool {
     }
 
     public void releaseConnection(Connection connection) throws ConnectionPoolException {
-        //add connection exception
         if (connection != null) {
             if (connection instanceof ConnectionProxy && givenAwayConnections.remove(connection)) {
                 try {
                     freeConnections.put((ConnectionProxy) connection);
                 } catch (InterruptedException e) {
-//                    LOGGER.log(Level.FATAL, "Connection is not a ProxyConnection");
+                    LOGGER.log(Level.FATAL, "Connection is not a ProxyConnection");
                     throw new ConnectionPoolException("Connection is not a ProxyConnection", e);
                 }
             }
         }
-
+        LOGGER.log(Level.INFO, "Connection release");
     }
 
     public void destroyPool() throws ConnectionPoolException {
@@ -78,7 +79,6 @@ public enum ConnectionPool {
                 if (!freeConnections.isEmpty()) {
                     ConnectionProxy connectionProxy = freeConnections.take();
                     connectionProxy.reallyClose();
-//                    freeConnections.take().reallyClose();
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -88,22 +88,13 @@ public enum ConnectionPool {
     }
 
     private void deregisterDrivers() throws ConnectionPoolException {
-//        DriverManager.getDrivers().asIterator().forEachRemaining(driver -> {
-//            try {
-//                DriverManager.deregisterDriver(driver);
-//            } catch (SQLException e) {
-//                //log
-//                e.printStackTrace();
-//            }
-//        });
-
         Enumeration<Driver> drivers = DriverManager.getDrivers();
         while (drivers.hasMoreElements()) {
             Driver driver = drivers.nextElement();
             try {
                 DriverManager.deregisterDriver(driver);
             } catch (SQLException e) {
-//                LOGGER.log(Level.ERROR, "Registered drivers are missing", e);
+                LOGGER.log(Level.ERROR, "Registered drivers are missing", e);
                 throw new ConnectionPoolException("Registered drivers are missing", e);
             }
         }
