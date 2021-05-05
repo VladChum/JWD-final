0990817b352c698f2302095772a4547096cc9b66
vlad <DegaDega001@mail.ru>
2021-05-05 19:38:59 +0300

    update exception

--- src/main/java/com/epam/jwd_final/exception/DaoException.java
+++ src/main/java/com/epam/jwd_final/exception/DaoException.java
@@ -3,6 +3,21 @@ package com.epam.jwd_final.exception;
 import java.sql.SQLException;
 
 public class DaoException extends Throwable {
-    public DaoException(SQLException e) {
+    private static final long serialVersionUID = 1L;
+
+    public DaoException() {
+        super();
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(Exception e) {
+        super(e);
+    }
+
+    public DaoException(String message, Exception e) {
+        super(message, e);
     }
 }
