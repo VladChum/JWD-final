d31854e208e89afb754582aa7b21600fe16b2057
vlad <DegaDega001@mail.ru>
2021-05-19 03:04:07 +0300

    add create user

--- src/main/java/com/epam/jwd_final/controller/command/impl/CreateUser.java
+++ src/main/java/com/epam/jwd_final/controller/command/impl/CreateUser.java
@@ -1,9 +1,13 @@
 package com.epam.jwd_final.controller.command.impl;
 
 import com.epam.jwd_final.controller.command.Command;
+import com.epam.jwd_final.entity.Status;
+import com.epam.jwd_final.entity.User;
+import com.epam.jwd_final.exception.ServiceException;
 import com.epam.jwd_final.service.AccountService;
 import com.epam.jwd_final.service.ServiceProvider;
 import com.epam.jwd_final.service.UserService;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
@@ -17,8 +21,31 @@ public class CreateUser implements Command {
     private final UserService userService = ServiceProvider.INSTANCE.getUserService();
     private final AccountService accountService = ServiceProvider.INSTANCE.getAccountService();
 
+    private final String LOGIN = "login";
+    private final String PASSWORD = "password";
+    private final String FIRST_NAME = "firstName";
+    private final String LAST_NAME = "lastName";
+    private final String PHONE = "phone";
+    private final String EMAIL = "email";
+
     @Override
     public void execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String login = req.getParameter(LOGIN);
+        String password = req.getParameter(PASSWORD);
+        String firstNme = req.getParameter(FIRST_NAME);
+        String lastName = req.getParameter(LAST_NAME);
+        String phone = req.getParameter(PHONE);
+        String email = req.getParameter(EMAIL);
 
+        try {
+            /**ToDo
+             * make status suspended where create user
+             * */
+            Long accountId = accountService.addAccount(login, password);
+            User user = new User(accountId, firstNme, lastName, phone, email, Status.ACTIVATE);
+            userService.addUser(user);
+        } catch (ServiceException e) {
+            LOGGER.log(Level.ERROR, e.getMessage() + " " + e);
+        }
     }
 }
